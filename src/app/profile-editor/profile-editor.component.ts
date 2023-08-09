90b7cbc98cbaf9ddd0af905cde6fece3f70ad3b7
Md. Zahid Hasan <zahid.hasan@brainstation-23.com>
2023-08-09 20:23:02 +0600

    add required validator

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
@@ -9,16 +10,16 @@ import { FormBuilder } from '@angular/forms';
 export class ProfileEditorComponent {
   constructor(private fb: FormBuilder) { }
 
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
-    })
-  })
+  profileForm = this.fb.group({
+    firstName: ['',Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+  });
 
 
   onSubmit() {
